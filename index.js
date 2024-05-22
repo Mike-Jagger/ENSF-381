ac6d462623659a84e995b1f9cca78514411aaa57
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-22 12:14:38 -0600

    experimenting with casting

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 "use strict";
-function add(num) {
-    let arrayOfNumbers = [];
-    while (num > 0) {
-        arrayOfNumbers.push(num);
-        num--;
-    }
-    console.log(arrayOfNumbers);
-    return arrayOfNumbers.reduce((factorial, currVal) => factorial * currVal, 1);
-}
-console.log(add(6));
+let x = 'hello';
+x = 5;
+console.log(x.length);
--- index.ts
+++ index.ts
@@ -1,13 +1,4 @@
-function add(num: number) {
-    let arrayOfNumbers: number[] | null = [];
-    
-    while (num > 0) {
-        arrayOfNumbers.push(num);
-      num--;
-    }
-    console.log(arrayOfNumbers);
-    
-    return arrayOfNumbers.reduce((factorial, currVal) => factorial * currVal, 1);
-  }
-  
-  console.log(add(6));
+let x: unknown = 'hello';
+x = 5;
+
+console.log((x as string).length);
