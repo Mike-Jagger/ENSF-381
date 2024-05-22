f5206f5d398b68b3c2f50c5534940dc34ea44cc4
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-22 02:22:39 -0600

    adding a test file to implement other code logic

---
+++ test.ts
@@ -0,0 +1,13 @@
+function add(num: number) {
+    let arrayOfNumbers: number[] | null = [];
+    
+    while (num > 0) {
+        arrayOfNumbers.push(num);
+      num--;
+    }
+    console.log(arrayOfNumbers);
+    
+    return arrayOfNumbers.reduce((factorial, currVal) => factorial * currVal, 1);
+  }
+  
+  console.log(add(5));
