390b960a6c8222113a9bbac465f008a658b9807d
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-05-22 01:49:46 -0600

    saving logic

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 "use strict";
-const myPhone = {
-    name: 'Samsung',
-    size: 20,
-    dateBought: 2020
-};
-console.log(myPhone);
-const anotherPhone = {
-    name: 'Apple',
-    size: 30,
-    dateBought: 2021
-};
-console.log(anotherPhone);
